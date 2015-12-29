8fd5af40951e3e14c643485d59993887e27df71f
System Administrator <root@Mirnas-iMac.local>
2015-12-29 13:49:36 -0600

    Add header for main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python
+http://www.kongregate.com/games/Volch/endless-expansion?haref=HP_TGTM_endless-expansion
+"""
+This is the main file for a script that reads info off a game on kongregate.com and acts upon it.
+
+"""
+
+__author__ = "Alex Flores Escarcega"
+__copyright__ = "Copyright 2007, Alex Flores Escarcega"
+__credits__ = ["Alex Flores Escarcega"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Alex Flores Escarcega"
+__email__ = "alex.floresescar@gmail.com"
+__status__ = "Development"
+
+# import line/s for builtin modules
+#import line/s for 3rd party
  
